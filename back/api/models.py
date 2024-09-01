e5ffe3c368ed976bc87e01e7a62c395b4d8c0be9
Artem Parshyn <parshyn4@gmail.com>
2024-09-01 18:13:20 +0300

    reklama

--- back/api/models.py
+++ back/api/models.py
@@ -3,4 +3,9 @@ from django.db import models
 
 
 class ApiUser(AbstractUser):
-    photo = models.ImageField(upload_to='images/%Y/%m/%d/', default=None)
+    photo = models.ImageField(upload_to='images/%Y/%m/%d/avatar', default=None)
+
+
+class Reklama(models.Model):
+    photo = models.ImageField(upload_to='images/%Y/%m/%d/reklama', default=None)
+    url = models.CharField(max_length=64)
